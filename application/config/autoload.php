72c4ebf4b7ffaeafaa0880da0529b6c80b4d93be
hrydi <hari.nube@gmail.com>
2015-04-06 15:59:08 +0700

    adding template file

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('pagination','form_validation','database','table','session','encrypt','template', 'menu');
 
 
 /*
@@ -64,7 +64,7 @@ $autoload['libraries'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array();
+$autoload['helper'] = array('html','form','url');
 
 
 /*
